afcd4e72965a4fc60dba2600c175a1a899edead9
Aaron Rice <adrice727@gmail.com>
2017-08-16 13:38:18 -0700

    Update five.

---
+++ user/unions.untyped.js
@@ -0,0 +1,19 @@
+import R from 'ramda';
+
+const updateBillingZipCode = (zipCode) => ({
+  type: 'UPDATE_USER_BILLING_ZIP_CODE', zipCode
+})
+
+const userReducer = (state = null, action) => {
+  switch (action.type) {
+    case 'LOGIN_USER':
+      return action.user;
+    case 'LOGOUT_USER':
+      return null;
+    case 'UPDATE_BILLLING_ZIP_CODE':
+      return R.assocPath(['address', 'zipCode'], action.zipCode, state);
+    default:
+      return state;
+  }
+}
+
