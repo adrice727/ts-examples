5d351f21e3a885c196583afedb6b34b411fef174
Aaron Rice <adrice727@gmail.com>
2017-08-22 12:26:11 -0700

    Fill in user class and types.

---
+++ global.d.ts
@@ -0,0 +1,4 @@
+declare type Name = { first: string, last: string, middleInitial?: string};
+declare type Email = { primary: string, secondary?: string };
+declare type Address = { street: string, streetAdditional?: string, city: string, state: string, zip: number};
+declare type Phone = { primary: string, secondary?: string };
--- user/user.ts
+++ user/user.ts
@@ -1,5 +1,65 @@
-export class User {
-
+export default class User {
+  name: Name
+  email: Email
+  address?: Address
+  phone?: Phone
+  constructor(name: Name, email: Email, address?: Address, phone?: Phone) {
+    this.name = name;
+    this.email = email;
+    this.address = address;
+    this.phone = phone;
+  }
 }
+
+const tim = new User(
+  { first: 'tim', last: 'pike', middleInitial: 'd' },
+  { primary : 'tim22@gmail.com' },
+  { street: '1222 Haight Street', streetAdditional: 'Apt 22', city: 'San Francisco', state: 'CA', zip: 94117 },
+  { primary: '5553838393' },
+);
+
+const susan = new User(
+  { first: 'susan', last: 'williamson' },
+  { primary : 'susq@gmail.com' },
+  { street: '787 Waller Street', city: 'San Francisco', state: 'CA', zip: 94114 },
+  { primary: '7872982929' },
+);
+
+const lili = new User(
+  { first: 'lili', last: 'halson' },
+  { primary : 'lilihal04@gmail.com' },
+)
+
+const peter = new User(
+  { first: 'peter', last: 'song' },
+  { primary : 'peterokok@gmail.com' },
+  { street: '373 Mandolin', streetAdditional: 'Apt 684', city: 'Oakland', state: 'CA', zip: 94608 },
+  { primary: '9456278716' },
+);
+
+const anna = new User(
+  { first: 'anna', last: 'klein' },
+  { primary : 'annaptok88@gmail.com' },
+  { street: '2322 20th Steet', streetAdditional: 'Apt 4A', city: 'San Francisco', state: 'CA', zip: 94110 },
+  { primary: '9456278716', secondary: '9457279992' },
+);
+
+const alberto = new User(
+  { first: 'alberto', last: 'guzman' },
+  { primary : 'slimalberto@gmail.com' },
+  { street: '440 Page Street', streetAdditional: 'Apt 1A', city: 'San Francisco', state: 'CA', zip: 94114 },
+);
+
+const users = [tim, susan, lili, peter, anna, alberto];
+
+const userZipCodes: number[] = users.reduce((acc: number[], user: User) => {
+  return (user && user.address) ? acc.concat(user.address.zip) : acc;
+}, []);
+
+const secondaryPhoneNumbers: string[] = users.reduce((acc: string[], user: User) => {
+  return (user.phone && user.phone.secondary) ? acc.concat(user.phone.secondary) : acc;
+}, []);
+
+const usersAroundLowerHaight = users.filter(users => users.address && users.address.zip === 94114)
 
 
