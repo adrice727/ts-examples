30e5a921b97e91a64a3f9aeec62ad8b2c14125ae
Aaron Rice <adrice727@gmail.com>
2017-08-22 12:55:38 -0700

    Update reducer example.

--- user/unions.ts
+++ user/unions.ts
@@ -1,9 +1,10 @@
 import * as R from 'ramda';
 import { User } from './user';
 
-const updateBillingZipCode: ActionCreator = (zipCode: ZipCode): UserAction => ({
-  type: 'UPDATE_USER_BILLING_ZIP_CODE', zipCode
-})
+const updateUserZipCode: ActionCreator = (zipCode: ZipCode): UserAction => ({
+  type: 'UPDATE_USER_ZIP_CODE',
+  zipCode,
+});
 
 const userReducer = (state = null, action: UserAction): null | User => {
   switch (action.type) {
@@ -11,16 +12,14 @@ const userReducer = (state = null, action: UserAction): null | User => {
       return action.user;
     case 'LOGOUT_USER':
       return null;
-    case 'UPDATE_USER_BILLING_ZIP_CODE':
+    case 'UPDATE_USER_ZIP_CODE':
       return R.assocPath(['address', 'zipCode'], action.zipCode, state);
     default:
       return state;
   }
-}
+};
 
 type UserAction =
-{ type: 'CREATE_USER', user: User } |
-{ type: 'LOGIN_USER', user: User } |
-{ type: 'LOGOUT_USER', user: User } |
-{ type: 'UPDATE_USER_BILLING_ZIP_CODE', zipCode: ZipCode};
-
+  | { type: 'LOGIN_USER'; user: User }
+  | { type: 'LOGOUT_USER'; user: User }
+  | { type: 'UPDATE_USER_ZIP_CODE'; zipCode: ZipCode };
