58b11786030cf60e4e3f9f3fd93c9b1a24bfc5ed
Aaron Rice <adrice727@gmail.com>
2017-08-22 11:51:01 -0700

    Create new user.

--- user/user.untyped.js
+++ user/user.untyped.js
@@ -25,3 +25,11 @@ class User {
     this.phone = phone;
   }
 }
+
+
+const tim = new User(
+  { first: 'tim', last: 'pike', middleInitial: 'd' },
+  { primary: 'tim22@gmail.com' },
+  { street: '1222 Haight Street', streetAdditional: 'Apt 22', city: 'San Francisco', state: 'CA', zip: 94117 },
+  { primary: '5553838393' },
+)
