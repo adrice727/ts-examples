7f212078c8237c4c379d33467b8cd3e2909c2edb
Aaron Rice <adrice727@gmail.com>
2017-08-24 16:51:33 -0700

    Cleanup.

--- user/user.ts
+++ user/user.ts
@@ -1,5 +1,3 @@
-import * as R from 'ramda';
-
 export class User {
   name: Name
   email: Email
@@ -19,48 +17,4 @@ const tim = new User(
   { street: '1222 Haight Street', streetAdditional: 'Apt 22', city: 'San Francisco', state: 'CA', zip: 94117 },
   { primary: '5553838393' },
 );
-
-const susan = new User(
-  { first: 'susan', last: 'williamson' },
-  { primary : 'susq@gmail.com' },
-  { street: '787 Waller Street', city: 'San Francisco', state: 'CA', zip: 94114 },
-  { primary: '7872982929' },
-);
-
-const lili = new User(
-  { first: 'lili', last: 'halson' },
-  { primary : 'lilihal04@gmail.com' },
-)
-
-const peter = new User(
-  { first: 'peter', last: 'song' },
-  { primary : 'peterokok@gmail.com' },
-  { street: '373 Mandolin', streetAdditional: 'Apt 684', city: 'Oakland', state: 'CA', zip: 94608 },
-  { primary: '9456278716' },
-);
-
-const anna = new User(
-  { first: 'anna', last: 'klein' },
-  { primary : 'annaptok88@gmail.com' },
-  { street: '2322 20th Steet', streetAdditional: 'Apt 4A', city: 'San Francisco', state: 'CA', zip: 94110 },
-  { primary: '9456278716', secondary: '9457279992' },
-);
-
-const alberto = new User(
-  { first: 'alberto', last: 'guzman' },
-  { primary : 'slimalberto@gmail.com' },
-  { street: '440 Page Street', streetAdditional: 'Apt 1A', city: 'San Francisco', state: 'CA', zip: 94114 },
-);
-
-const users = [tim, susan, lili, peter, anna, alberto];
-
-const userZipCodes: number[] = users.reduce((acc: number[], user: User) => {
-  return (user && user.address) ? acc.concat(user.address.zip) : acc;
-}, []);
-
-const secondaryPhoneNumbers: string[] = users.reduce((acc: string[], user: User) => {
-  // ???
-}, []);
-
-const usersAroundLowerHaight = users.filter(users => users.address && users.address.zip === 94114)
 
