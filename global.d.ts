3549d0353d3a67904c5f66f031b2c402181f2bf4
Aaron Rice <adrice727@gmail.com>
2017-08-24 17:37:33 -0700

    Cleanup types.'

--- global.d.ts
+++ global.d.ts
@@ -1,4 +1,4 @@
-declare type Name = { first: string, last: string, middleInitial?: string};
-declare type Email = { primary: string, secondary?: string };
-declare type Address = { street: string, streetAdditional?: string, city: string, state: string, zip: number};
-declare type Phone = { primary: string, secondary?: string };
+type Name = { first: string, last: string, middleInitial?: string};
+type Email = { primary: string, secondary?: string };
+type Address = { street: string, streetAdditional?: string, city: string, state: string, zip: number};
+type Phone = { primary: string, secondary?: string };
