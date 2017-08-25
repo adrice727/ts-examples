f7ff28f6862a41d9c53454032acf632840fcaa17
Aaron Rice <adrice727@gmail.com>
2017-08-24 17:36:46 -0700

    No declare.

--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,5 @@
-declare type ZipCode = number;
-declare type Name = { first: string, last: string, middleInitial?: string};
-declare type Email = { primary: string, secondary?: string };
-declare type Address = { street: string, streetAdditional?: string, city: string, state: string, zip: ZipCode};
-declare type Phone = { primary: string, secondary?: string };
+type ZipCode = number;
+type Name = { first: string, last: string, middleInitial?: string};
+type Email = { primary: string, secondary?: string };
+type Address = { street: string, streetAdditional?: string, city: string, state: string, zip: ZipCode};
+type Phone = { primary: string, secondary?: string };
